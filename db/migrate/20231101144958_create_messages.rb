f89bd184a57d1846f7f85bc5f23eaac0c936e596
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-01 14:55:17 +0000

    [Add] Activityモデルとテーブルを作成

--- db/migrate/20231101144958_create_messages.rb
+++ db/migrate/20231101144958_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[6.1]
     create_table :messages do |t|
       t.integer :user_id,     null: false
       t.integer :chatroom_id, null: false
+      t.boolean :readed_flag, null: false, default: false
       t.text :body,           null: false, defalut: ""
 
       t.timestamps
