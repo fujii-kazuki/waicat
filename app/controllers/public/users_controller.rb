48c7e3bc90e2d0e01a9eead2d5806ce197f67627
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-01 20:59:11 +0000

    [Add] 退会処理を追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,4 +7,10 @@ class Public::UsersController < ApplicationController
 
   def check
   end
+
+  def leave
+    current_user.update(deleted_flag: true)
+    reset_session
+    redirect_to root_path, notice: '退会処理が完了しました。ご利用ありがとうございました。'
+  end
 end
