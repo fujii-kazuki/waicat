6b870f2e020c46effc6dc9377cf4e60079258a67
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-02 07:30:28 +0000

    [Add] 会員ログイン時に削除フラグ判定の処理を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_inactive_user, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -34,4 +35,22 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  protected
+
+  # 会員ログイン時の削除フラグ判定
+  def reject_inactive_user
+    user = User.find_by(email: params[:user][:email])
+    if user
+      if user.valid_password?(params[:user][:password]) && user.deleted_flag
+        flash[:danger] = 'お客様は退会済みです。申し訳ございませんが、再度新規登録をお願いします。'
+        redirect_to new_user_registration_path
+      end
+    end
+  end
+  
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
