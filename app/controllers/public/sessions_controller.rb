3ca29613b6a8a38962e8dafd005337c152ae0e0a
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-02 07:20:34 +0000

    [Update] 会員ログイン・ログアウト後のリダイレクト先を指定

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,14 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # ログイン後にマイページへリダイレクト
+  def after_sign_in_path_for(resource)
+    users_my_page_path
+  end
+
+  # ログアウト後にトップページへリダイレクト
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
