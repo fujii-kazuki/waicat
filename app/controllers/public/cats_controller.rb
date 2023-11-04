26ee8f3967e8fc48abf24103866d99016b360f43
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-04 06:01:30 +0000

    [Add] 猫ちゃん新規投稿ページを作成

--- app/controllers/public/cats_controller.rb
+++ app/controllers/public/cats_controller.rb
@@ -10,14 +10,50 @@ class Public::CatsController < ApplicationController
   end
 
   def new
+    @cat = Cat.new
   end
 
   def edit
   end
 
   def confirm
+    @cat = Cat.new(cat_params)
+    @cat.user_id = current_user.id
+    @cat.publication_date = Time.zone.now  # 仮の掲載日
+    @cat.publication_status = 0
+
+    if @cat.invalid?
+      render :new
+    end
   end
 
   def thanks
+  end
+
+  private
+  
+  # ストロングパラメーター
+  def cat_params
+    params.require(:cat).permit(
+      :publication_title,
+      :name,
+      :age,
+      :gender,
+      :weight,
+      :breed,
+      :animal_print,
+      :hair_length,
+      :castration_flag,
+      :vaccine_flag,
+      :postal_code,
+      :prefecture,
+      :municipalitie,
+      :background,
+      :personality,
+      :health,
+      :delivery_place,
+      :remarks,
+      :publication_deadline
+    )
   end
 end
