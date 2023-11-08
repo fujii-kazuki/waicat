c5eb61bcb45251a5e2fc734c06c081df3767e4e2
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-08 21:57:34 +0900

    [Add] 掲載ブックマーク機能を実装

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,4 +1,16 @@
 class Public::BookmarksController < ApplicationController
   def index
   end
+
+  def create
+    @cat = Cat.find(params[:cat_id])
+    current_user.bookmarks.create(cat_id: params[:cat_id])
+    flash[:notice] = 'ブックマークに登録しました。'
+  end
+
+  def destroy
+    @cat = Cat.find(params[:id])
+    current_user.bookmarks.find_by(cat_id: params[:id]).destroy
+    flash[:notice] = 'ブックマークを解除しました。'
+  end
 end
