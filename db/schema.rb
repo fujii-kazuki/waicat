4ad46d25776580df595a552b1582297880b34524
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-09 15:06:26 +0900

    [Add] Prefectureモデルとテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_07_152551) do
+ActiveRecord::Schema.define(version: 2023_11_09_060440) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -157,6 +157,12 @@ ActiveRecord::Schema.define(version: 2023_11_07_152551) do
     t.text "body", default: "", null: false
     t.boolean "readed_flag", default: false, null: false
     t.boolean "deleted_flag", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
