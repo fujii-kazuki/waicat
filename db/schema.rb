e64dbec09c46f6ad0a7c11af4c4e48f323ff0d63
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-11 17:50:35 +0900

    [Change] chatroomsテーブルにdeleted_flagカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2023_11_09_060440) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.integer "candidate_id", null: false
+    t.boolean "deleted_flag", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
