990ac80566da3f603e9bd745dc0a4fb336ee2aaa
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-12 14:52:03 +0900

    [Add] チャットルーム一覧、詳細ページを作成

--- app/controllers/public/chatrooms_controller.rb
+++ app/controllers/public/chatrooms_controller.rb
@@ -1,5 +1,9 @@
 class Public::ChatroomsController < ApplicationController
   def index
+    @user = current_user
+    @chatrooms = @user.chatroom_users.map { |chatroom_user| chatroom_user.chatroom }
+    @active_chatrooms = @chatrooms.select { |chatroom| chatroom.deleted_flag == false }
+    @passive_chatrooms = @chatrooms.select { |chatroom| chatroom.deleted_flag == true }
   end
 
   def show
