68c8df65c188726f30b7a221b99a4e2e92923a11
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-14 03:58:47 +0900

    [Add] 管理者チャットルーム削除機能を実装

--- app/controllers/admin/chatrooms_controller.rb
+++ app/controllers/admin/chatrooms_controller.rb
@@ -16,4 +16,11 @@ class Admin::ChatroomsController < ApplicationController
     @recruiter = @chatroom.chat_partner(@candidater) #里親募集者
     @cat = @chatroom.candidate.cat
   end
+
+  def leave
+    chatroom = Chatroom.find(params[:chatroom_id])
+    chatroom.update(deleted_flag: true)
+    flash[:notice] = 'このチャットルームを閉じました。'
+    redirect_to admin_chatroom_path(chatroom.id)
+  end
 end
