350c9a34f7828a0ece275edf4fc98660e4347809
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-14 03:26:37 +0900

    [Add] 会員毎の管理者チャットルーム一覧ページを作成

--- app/controllers/admin/chatrooms_controller.rb
+++ app/controllers/admin/chatrooms_controller.rb
@@ -1,6 +1,12 @@
 class Admin::ChatroomsController < ApplicationController
   def index
-    @chatrooms = Chatroom.all.order(created_at: :desc)
+    if params[:user_id]
+      chatroom_users = ChatroomUser.where(user_id: params[:user_id])
+      @chatrooms = chatroom_users.map { |chatroom_user| chatroom_user.chatroom }
+      @chatrooms.reverse
+    else
+      @chatrooms = Chatroom.all.order(created_at: :desc)
+    end
   end
 
   def show
