cb75afc836fee36b41884bd09a3c407ac19bb6fd
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-14 02:53:12 +0900

    [Add] 管理者掲載コメント削除機能を実装

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -6,4 +6,11 @@ class Admin::CommentsController < ApplicationController
       @comments = Comment.where(user_id: params[:user_id]).order(created_at: :desc)
     end
   end
+
+  def leave
+    comment = Comment.find(params[:comment_id])
+    comment.update(deleted_flag: true)
+    flash[:notice] = "コメント「#{comment.body.truncate(30)}」を削除しました。"
+    redirect_back fallback_location: root_path
+  end
 end
