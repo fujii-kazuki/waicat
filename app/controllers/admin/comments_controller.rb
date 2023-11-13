a0649ac6d72370b8945f27f02e030aaf50e7232d
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-14 02:39:39 +0900

    [Add] 会員毎の管理者掲載コメント一覧ページを作成

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,6 +2,8 @@ class Admin::CommentsController < ApplicationController
   def index
     if params[:cat_id]
       @comments = Comment.where(cat_id: params[:cat_id]).order(created_at: :desc)
+    elsif params[:user_id]
+      @comments = Comment.where(user_id: params[:user_id]).order(created_at: :desc)
     end
   end
 end
