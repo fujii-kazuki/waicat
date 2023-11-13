8fec663ce2afaf83fb0c6760837d19f695ee01d3
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-13 20:06:59 +0900

    [Add] お問い合わせ送信機能を実装

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -2,4 +2,21 @@ class Public::ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.user_id = current_user.id
+    if @contact.save
+      flash[:notice] = 'お問い合わせを受け付けました。'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:title, :body)
+  end
 end
