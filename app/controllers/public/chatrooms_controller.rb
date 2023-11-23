ead02bd4b019b6d569aba7bc7672eb56249b323e
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-23 09:17:03 +0900

    [Add] チャットルーム一覧ページをスタイリング

--- app/controllers/public/chatrooms_controller.rb
+++ app/controllers/public/chatrooms_controller.rb
@@ -1,9 +1,11 @@
 class Public::ChatroomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_guest_user, if: :user_signed_in?
+
   def index
     @user = current_user
     @chatrooms = @user.chatroom_users.map { |chatroom_user| chatroom_user.chatroom }
-    @active_chatrooms = @chatrooms.select { |chatroom| chatroom.deleted_flag == false }
-    @passive_chatrooms = @chatrooms.select { |chatroom| chatroom.deleted_flag == true }
+    @chatrooms.sort! { |cr_a, cr_b| cr_b.messages.first.created_at <=> cr_a.messages.first.created_at }
   end
 
   def show
