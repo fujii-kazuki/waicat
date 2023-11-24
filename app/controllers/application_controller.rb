f9cd108f8d547b1e362e8c1c322af99513690895
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-11-25 06:27:04 +0900

    [Add] 未読通知数をナビゲーションに表示する機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   before_action :authenticate_admin!, if: :admin_url?
-  before_action :check_publication_period
+  before_action :check_unread_notices, if: proc { user_signed_in? && !current_user.is_guest_user? }
+  before_action :check_publication_period, if: proc { user_signed_in? && !current_user.is_guest_user? }
   
   # ゲストユーザーか確認
   def check_guest_user
@@ -15,10 +16,14 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # 未読通知数を確認
+  def check_unread_notices
+    @unread_notices_num = current_user.notices.where(readed_flag: false).count
+  end
+
   # 掲載期間を確認
   def check_publication_period
     cats = Cat.where(publication_status: 'public').where('publication_deadline < ?', Time.zone.now)
     cats.update_all(publication_status: 'publication_deadline')
   end
-
 end
