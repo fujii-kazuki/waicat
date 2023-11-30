225d21c4785e2d012d8eabfd332e7bfe70532f6e
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-12-01 03:48:43 +0900

    [Add] 管理者お問い合わせ一覧ページをスタイリング

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ContactsController < ApplicationController
   def index
+    @q = Contact.ransack(params[:q])
+    @contacts = @q.result.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def new
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,11 @@ class Contact < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+
+  private
+
+  # Gem「ransack」の検索対象カラムをホワイトリストに登録
+  def self.ransackable_attributes(auth_object = nil)
+    ['id', 'readed_flag', 'created_at']
+  end
 end
