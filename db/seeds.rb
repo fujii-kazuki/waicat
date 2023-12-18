d989bd9bfc0081db9c84891c88c0262595d35d47
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-12-19 05:55:30 +0900

    [Clean] Contactモデルの初期レコード作成ソースをリファクタリング

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,77 +34,7 @@ require seeds_path('animal_print')
 # 里親募集掲載
 require seeds_path('cat')
 
-# 猫ちゃん
-# 15.times do |i|
-#   i += 1
-#   publication_date = Time.zone.now
-#   publication_deadline = publication_date.since(rand(2..14).days)
-
-#   cat = Cat.new(
-#     user_id: i,
-#     publication_title: "タイトル-#{i}",
-#     name: "#{i}にゃん",
-#     age: Random.rand(12.5).ceil(1),
-#     gender: rand(0..1),
-#     weight: Random.rand(8.5).ceil(1),
-#     breed: breed_names.sample,
-#     animal_print: animal_print_names.sample,
-#     hair_length: rand(0..1),
-#     castration_flag: [true, false].sample,
-#     vaccine_flag: [true, false].sample,
-#     postal_code: '1234567',
-#     prefecture: Prefecture.find(rand(1..47)).name,
-#     city: "にゃん#{i}町",
-#     background: '里親募集の経緯',
-#     personality: '性格',
-#     health: '健康状態',
-#     delivery_place: '譲渡場所',
-#     remarks: '備考',
-#     publication_date: publication_date,
-#     publication_deadline: publication_deadline,
-#     publication_status: rand(0..6)
-#   )
-#   # 画像を登録
-#   cat.photos.attach(io: File.open(Rails.root.join("app/assets/images/cats/img-1.jpg")), filename: "img-1.jpg")
-#   cat.photos.attach(io: File.open(Rails.root.join("app/assets/images/cats/img-2.jpg")), filename: "img-2.jpg")
-#   cat.photos.attach(io: File.open(Rails.root.join("app/assets/images/cats/img-3.jpg")), filename: "img-3.jpg")
-#   cat.save!
-# end
-
-# # コメント
-# 2.times do |i|
-#   i += 1
-
-#   5.times do |j|
-#     j += 1
-
-#     Comment.create!(
-#       cat_id: i,
-#       user_id: j,
-#       body: "#{User.find(j).name}のテストコメント"
-#     )
-#   end
-# end
-
-# # 通知
-# 15.times do |i|
-#   i += 1
-#   Notice.create!(
-#     user_id: rand(1..2),
-#     title: "通知-#{i}",
-#     body: "通知内容です-#{i}。通知内容です-#{i}。通知内容です-#{i}。通知内容です-#{i}。通知内容です-#{i}。
-#           通知内容です-#{i}。通知内容です-#{i}。通知内容です-#{i}。",
-#   )
-# end
-
-# # お問い合わせ
-# 15.times do |i|
-#   i += 1
-#   Contact.create!(
-#     user_id: i,
-#     title: "お問い合わせタイトル-#{i}",
-#     body: "#{i}-お問い合わせ内容ですお問い合わせ内容ですお問い合わせ内容ですお問い合わせ内容ですお問い合わせ内容です"
-#   )
-# end
+# お問い合わせ
+require seeds_path('contact')
 
 puts 'seedの実行を完了'
