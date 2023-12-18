57de76eada8cbf44ac86ed46e0b93e83c1d2d043
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-12-18 17:44:10 +0900

    [Clean] AnimalPrintモデルの初期レコード作成ソースをリファクタリング

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,26 +28,9 @@ require seeds_path('user')
 # 種類
 require seeds_path('breed')
 
-
 # 毛の柄
-animal_print_names = [
-  '白（ホワイト）',
-  '黒（ブラック）',
-  '灰色（グレー）',
-  'キジトラ（ブラウンタビー）',
-  'サバトラ（シルバータビー）',
-  '茶トラ（レッドタビー）',
-  '黒白／白黒（ブラック＆ホワイト／ホワイト＆ブラック）',
-  '三毛（キャリコ）',
-  'サビ（トーティシェル）',
-  'キジ白（ブラウンタビー&ホワイト）',
-  'サバ白（シルバータビー&ホワイト）',
-  '茶白（レッドタビー&ホワイト）',
-  'その他'
-]
-animal_print_names.each do |animal_print_name|
-  AnimalPrint.create!(name: animal_print_name)
-end
+require seeds_path('animal_print')
+
 
 # 猫ちゃん
 15.times do |i|
